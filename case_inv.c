efd3dd478e95d5d36704d7615bdf443b826e5c57
Leonardo Pablo Castro Rezende <leonardo.pablo@terra.com.br>
2016-05-10 15:56:52 -0300

    incluído case_inv.c

---
+++ case_inv.c
@@ -0,0 +1,16 @@
+/*
+ *Programa: case_inv.c
+ *Inverte a caixa dos caracteres de uma string
+ *autor: lpcrezende
+ */
+
+#include <stdio.h>
+int main(argc, argv)
+	int argc;
+	char *argv[];
+{
+    if(argc != 2){
+        printf("Uso: %s ccccccc onde cccccc eh a string de entrada.\n", argv[0]);
+    }
+
+}
